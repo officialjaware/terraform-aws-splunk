f70ca84c068628ba4ec01d451fcddd54abe6c8bd
moksha-io <jaware@hashicorp.com>
2021-12-22 17:35:41 -0500

    updated download url

--- splunk-resources/installsplunkasrootuser.sh
+++ splunk-resources/installsplunkasrootuser.sh
@@ -29,8 +29,7 @@ hostname=splunk
 
 # VERSION 8.2.0
 splunkpackagefilename=splunk-8.2.0.tgz
-splunkpackagedownload="wget -O $splunkpackagefilename https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.2.0&product=splunk&filename=splunk-8.2.0-e053ef3c985f-Linux-x86_64.tgz&wget=true"
-
+splunkpackagedownload="wget -O $splunkpackagefilename https://download.splunk.com/products/splunk/releases/8.2.0/linux/splunk-8.2.0-e053ef3c985f-Linux-x86_64.tgz"
 
 # Installs wget
 if [ ! -x /usr/bin/wget ] ; then                                                                          
